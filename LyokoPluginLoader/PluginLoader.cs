a29793213f3023f3770262cc1465c47ca09b4f8c
GoodOldJack12 <deloght.jonathan@gmail.com>
2018-10-30 23:35:10 +0100

    Initial loader

---
+++ LyokoPluginLoader/PluginLoader.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Reflection;
+using LyokoAPI.Plugin;
+
+namespace LyokoPluginLoader
+{
+    //TODO: onGameStart(), DisableAll(), EnableAll()
+  public class PluginLoader
+  {
+      private DirectoryInfo pluginDirectory;
+      public List<LyokoAPIPlugin> Plugins;
+      public PluginLoader(string path)
+      {
+          if (getOrCreateDirectory(path, out pluginDirectory))
+          {
+              LoadPlugins();
+          }
+      }
+
+
+
+
+      private bool getOrCreateDirectory(string path, out DirectoryInfo directory)
+      {
+          if (File.Exists(path))
+          {
+              Console.WriteLine("The given plugin directory is a file!");
+              directory = null;
+              return false;
+          }else if (Directory.Exists(path))
+          {
+              directory = new DirectoryInfo(path);
+              return true;
+          }
+          else
+          {
+              directory =  Directory.CreateDirectory(path);
+              return true;
+          }
+      }
+
+      private void LoadPlugins()
+      {
+          string[] pluginFiles = Directory.GetFiles(pluginDirectory.FullName);
+          List<LyokoAPIPlugin> UnloadedPlugins = new List<LyokoAPIPlugin>();
+          UnloadedPlugins = (
+              // From each file in the files.
+              from file in pluginFiles
+              // Load the assembly.
+              let asm = Assembly.LoadFile(file)
+              // For every type in the assembly that is visible outside of
+              // the assembly.
+              from type in asm.GetExportedTypes()
+              // Where the type implements the interface.
+              where typeof(LyokoAPIPlugin).IsAssignableFrom(type)
+              // Create the instance
+              select (LyokoAPIPlugin) Activator.CreateInstance(type)
+          ).ToList();
+
+          foreach (var unloadedPlugin in UnloadedPlugins)
+          {
+              if (unloadedPlugin.OnEnable())
+              {
+                  Plugins.Add(unloadedPlugin);
+              }
+              else
+              {
+                  Console.WriteLine("The plugin: {0} by {1} couldn't be loaded!",unloadedPlugin.GetName(),unloadedPlugin.GetAuthor());
+              }
+          }
+      }
+
+  }
+}
